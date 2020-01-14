29a7ad79cd7c67082bc4a07d3c591f5c3c613c85
rupali <rupalipareek.3008@gmail.com>
2020-01-14 09:29:33 +0530

    Add Fucntion to create task

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 let todoItems = [];
-function addTodo(task) {
+
+function addtodo(task) {
     const todo = {
         task,
         checked: false,
         id: Date.now(),
     }
-    todoItems.push(task);
+    todoItems.push(todo);
     console.log(todoItems);
-    const form = document.querySelector('.my-form');
-    form.addEventListener('submit', event => {
-        event.preventDefault();
-        const input = document.querySelector('.js-input');
-        console.log(input);
-        const task = input.value.trim();
-        if (text != '') {
-            addTodo(task);
-            input.value = '';
-            input.focus();
+    const list = document.querySelector('.todo-list');
+    list.addEventListener('click', event => {
+        if (event.target.classList.contains('js-tick')) {
+            const itemKey = event.target.parentElement.dataset.key;
+            toggleDone(itemKey);
         }
     });
+    list.insertAdjacentHTML('afterend', `
+        <li data-key="${todo.id}">
+            <input id="${todo.id}" type="checkbox" />
+            <label for="${todo.id}" class="tick js-tick"></label>
+            <span>
+                ${todo.task}
+            </span>
+                <button class="delete-todo js-delete-todo">
+                    <svg><use href="#delete-icon"></use></svg>
+                </button>
+            </label>
+        </li>
+    `
+    )
 }
+
+const form = document.querySelector('.todo-from');
+form.addEventListener('submit', event => {
+    console.log("submitted");
+    event.preventDefault();
+    const input = document.querySelector('.todo-input');
+    const task = input.value.trim();
+    console.log(task);
+    if (task !== '') {
+        addtodo(task);
+        input.value = '';
+        //input.focus();
+    }
+});
